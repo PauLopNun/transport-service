18a3f6df00f1c0d6a4b8e249209d40b84dbea53d
guanaco1710 <ivancarmoneta@gmail.com>
2026-05-04 09:15:00 +0200

    feat: implement TruckId value object

--- src/main/java/com/gft/transport/truck/domain/TruckId.java
+++ src/main/java/com/gft/transport/truck/domain/TruckId.java
@@ -1,1 +1,9 @@
 package com.gft.transport.truck.domain;
+
+import java.util.UUID;
+
+public record TruckId(UUID value) {
+    public static TruckId generate() {
+        return new TruckId(UUID.randomUUID());
+    }
+}
