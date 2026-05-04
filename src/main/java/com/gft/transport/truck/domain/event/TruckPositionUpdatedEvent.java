59903181f23e9e20c30bd7ac110a859afb0d787b
guanaco1710 <ivancarmoneta@gmail.com>
2026-05-04 14:35:00 +0200

    feat: implement TruckPositionUpdatedEvent domain event

--- src/main/java/com/gft/transport/truck/domain/event/TruckPositionUpdatedEvent.java
+++ src/main/java/com/gft/transport/truck/domain/event/TruckPositionUpdatedEvent.java
@@ -1,1 +1,15 @@
 package com.gft.transport.truck.domain.event;
+
+import com.gft.transport.truck.domain.Location;
+import com.gft.transport.truck.domain.TruckId;
+import lombok.Value;
+
+import java.time.Instant;
+
+@Value
+public class TruckPositionUpdatedEvent {
+    TruckId truckId;
+    Location oldPosition;
+    Location newPosition;
+    Instant timestamp;
+}
