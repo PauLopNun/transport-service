6a3ea380b028b1cd61b4733db11a380a31f20663
guanaco1710 <ivancarmoneta@gmail.com>
2026-05-04 14:18:00 +0200

    feat: implement TruckRegisteredEvent domain event

--- src/main/java/com/gft/transport/truck/domain/event/TruckRegisteredEvent.java
+++ src/main/java/com/gft/transport/truck/domain/event/TruckRegisteredEvent.java
@@ -1,1 +1,16 @@
 package com.gft.transport.truck.domain.event;
+
+import com.gft.transport.truck.domain.Location;
+import com.gft.transport.truck.domain.TruckId;
+import lombok.Value;
+
+import java.time.Instant;
+
+@Value
+public class TruckRegisteredEvent {
+    TruckId truckId;
+    String name;
+    Location location;
+    int capacity;
+    Instant timestamp;
+}
