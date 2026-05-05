e9204d19137569a4ff2f1071bcc0e6ef862cc630
pulz <paulopeznunez@gmail.com>
2026-05-05 13:02:36 +0200

    docs: add OpenAPI @Schema annotation to TruckStatus

--- src/main/java/com/gft/transport/truck/domain/TruckStatus.java
+++ src/main/java/com/gft/transport/truck/domain/TruckStatus.java
@@ -1,5 +1,10 @@
 package com.gft.transport.truck.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Operational status of a truck in the fleet")
 public enum TruckStatus {
-    AVAILABLE, IN_TRANSIT, DELIVERED
+    AVAILABLE,
+    IN_TRANSIT,
+    DELIVERED
 }
