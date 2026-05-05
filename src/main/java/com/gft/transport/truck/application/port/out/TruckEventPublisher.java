c90025b889bb09c95b114a23bd83e6588ee6ab33
pulz <paulopeznunez@gmail.com>
2026-05-05 10:12:00 +0200

    feat: define TruckEventPublisher and DeliveryEventPublisher output ports

--- src/main/java/com/gft/transport/truck/application/port/out/TruckEventPublisher.java
+++ src/main/java/com/gft/transport/truck/application/port/out/TruckEventPublisher.java
@@ -1,1 +1,11 @@
 package com.gft.transport.truck.application.port.out;
+
+import com.gft.transport.truck.domain.event.TruckPositionUpdatedEvent;
+import com.gft.transport.truck.domain.event.TruckRegisteredEvent;
+import com.gft.transport.truck.domain.event.TruckStatusChangedEvent;
+
+public interface TruckEventPublisher {
+    void publish(TruckRegisteredEvent event);
+    void publish(TruckStatusChangedEvent event);
+    void publish(TruckPositionUpdatedEvent event);
+}
